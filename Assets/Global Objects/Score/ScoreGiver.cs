2dc3247404bb89fb53b01f7c4e018a83d30bd2e0
golergka <golergka@gmail.com>
2012-11-07 20:31:24 +0400

    ScoreGiver simplification

--- Assets/Global Objects/Score/ScoreGiver.cs
+++ Assets/Global Objects/Score/ScoreGiver.cs
@@ -4,40 +4,23 @@ using System.Collections.Generic;
 
 public class ScoreGiver : MonoBehaviour {
 	
-	List<GameObject> gifted = new List<GameObject>();
-	
 	public int score = 50;
-	
-	void OnEnable() {
-		
-		gifted.Clear();
-		
-	}
 
 	void OnCollisionEnter(Collision collisionInfo) {
 		
-		GameObject collidedWith = collisionInfo.collider.gameObject;
-		
-		GiveScore (collidedWith, collisionInfo.contacts[0].point);
+		GiveScore ( collisionInfo.contacts[0].point);
 		
 	}
 	
 	void OnTriggerEnter(Collider other) {
 		
-		GiveScore(other.gameObject, other.transform.position);
+		GiveScore( other.transform.position);
 		
 	}
 	
-	void GiveScore(GameObject collidedWith, Vector3 position) {
-		
-		if (gifted.Contains(collidedWith))
-			return;
-		
-		if (collidedWith.tag != "Ball")
-			return;
+	void GiveScore(Vector3 position) {
 		
 		LevelManager.instance.AddScore(score, position);
-		gifted.Add (collidedWith);
 		
 	}
 	
