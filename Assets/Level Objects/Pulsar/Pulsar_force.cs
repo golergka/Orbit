b3c7f961175ad762e929fc17b37a53f9906d93dc
Igor Fomichev <kocs.project@gmail.com>
2012-11-30 00:36:59 +0400

    Начало работ над пульсаром

---
+++ Assets/Level Objects/Pulsar/Pulsar_force.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pulsar_force : MonoBehaviour {
+
+	void OnCollisionEnter ()
+	{
+		// if (object_collision.gameObject.CompareTag ("Ball"))
+	//{
+       Debug.Log("hit");
+      // }
+	}
+}
